5ad66a09af71b14f89ae6e2816ba2900ab8ec6bb
luchesar <luchesar@ef5bcf5d-c752-0410-8205-f3954bfcf1e5>
2008-09-10 11:44:01 +0000

    little change in the example

--- trunk/resources/java/Chapter-11-Exceptions-Handling/ReadFile.java
+++ trunk/resources/java/Chapter-11-Exceptions-Handling/ReadFile.java
@@ -1,28 +1,37 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class ReadFile {
-	public static void main(String... a) throws IOException {
-		readFile("C:\\tools\\eclipse\\eclipse.ini");
+	public static void main(String[] args) {
+		try {
+			readFile("C:\\tools\\eclipse\\eclipse.ini");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void readFile(String fileName) throws IOException {
-		FileInputStream fis = new FileInputStream(fileName);
+		FileInputStream fis = null;
+		BufferedReader in = null;
 		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			try {
-				String tmp = null;
-				while ((tmp = in.readLine()) != null) {
-					System.out.println(tmp);
-				}
-			} finally {
+			fis = new FileInputStream(fileName);
+			in = new BufferedReader(new InputStreamReader(fis));
+			String tmp = null;
+			while ((tmp = in.readLine()) != null) {
+				System.out.println(tmp);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("The file \"" + fileName + "\" does not exist!");
+		} finally {
+			if (in != null) {
 				in.close();
 			}
-		} finally {
-			fis.close();
+			if (fis != null) {
+				fis.close();
+			}
 		}
 	}
 }
