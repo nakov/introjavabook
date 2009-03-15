f6b9e344867a4b4a46b7f275b50edc557512bf85
svetlin@nakov.com <svetlin@nakov.com@ef5bcf5d-c752-0410-8205-f3954bfcf1e5>
2009-03-15 21:16:48 +0000

    Added captcha bot spam protection. Minor bug fixes

--- web-site/introjavabook-bugs/utils.php
+++ web-site/introjavabook-bugs/utils.php
@@ -1,9 +1,5 @@
 <?php
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'user');
-	define('DB_PASS', 'pass');
-	define('DB_NAME', 'db');
-	define('MAIL_RECIPIENTS', 'introjavabook@nakov.com');
+	require_once("settings.php");
 	
 	function ConnectToDB()
 	{
@@ -15,30 +11,30 @@
 	}
 	
 	function AppendBugToDB($sender_name, $sender_email, $page_number, $row_number,
-		$bug_description, $sender_agreed_to_be_listed)
+		$bug_description, $sender_agree_to_be_listed)
 	{
-		$sender_name = mysql_real_escape_string($sender_name);
-		$sender_email = mysql_real_escape_string($sender_email);
-		$page_number = mysql_real_escape_string($page_number);
-		$row_number = mysql_real_escape_string($row_number);
-		$bug_description = mysql_real_escape_string($bug_description);
-		$sender_agreed_to_be_listed = mysql_real_escape_string($sender_agreed_to_be_listed);
+		$sender_name = MySQLEscapeString($sender_name);
+		$sender_email = MySQLEscapeString($sender_email);
+		$page_number = MySQLEscapeString($page_number);
+		$row_number = MySQLEscapeString($row_number);
+		$bug_description = MySQLEscapeString($bug_description);
+		$sender_agree_to_be_listed = MySQLEscapeString($sender_agree_to_be_listed);
 		$insert_query = 
 			"INSERT INTO introjavabook_bugs ".
 			"(sent_by, sender_email, page_number, row_number, description, ".
-			"sender_agreed_to_be_listed) ".
+			"sender_agree_to_be_listed) ".
 			"VALUES ('$sender_name', '$sender_email', '$page_number', '$row_number', ".
-			" '$bug_description', '$sender_agreed_to_be_listed')";
+			" '$bug_description', '$sender_agree_to_be_listed')";
 		$sql_result = mysql_query($insert_query);
 		return $sql_result;	
 	}
 	
 	function AppendCommentToDB($bug_id, $sender_name, $sender_email, $comment)
 	{
-		$bug_id = mysql_real_escape_string($bug_id);
-		$sender_name = mysql_real_escape_string($sender_name);
-		$sender_email = mysql_real_escape_string($sender_email);
-		$comment = mysql_real_escape_string($comment);
+		$bug_id = MySQLEscapeString($bug_id);
+		$sender_name = MySQLEscapeString($sender_name);
+		$sender_email = MySQLEscapeString($sender_email);
+		$comment = MySQLEscapeString($comment);
 		$insert_query = 
 			"INSERT INTO introjavabook_bug_comments (bug_id, sent_by, sender_email, comment) ".
 			"VALUES ('$bug_id', '$sender_name', '$sender_email', '$comment') ";
@@ -47,14 +43,16 @@
 	}
 	
 	function SendEmailNotificationsForBug($bug_id, $sender_name, $sender_email, $page_number,
-		$row_number, $bug_description, $sender_agreed_to_be_listed)
+		$row_number, $bug_description, $sender_agree_to_be_listed)
 	{
 		$date = date('d-m-Y H:i:s');
-		$agreed = "е";
-		if (! $sender_agreed_to_be_listed ) {
-			$agreed = "НЕ е";
+		$agree = "е";
+		if (! $sender_agree_to_be_listed ) {
+			$agree = "НЕ е";
 		}
-		//$bug_description = stripslashes($bug_description);
+
+		$bug_description = RemoveUnwantedSlashes($bug_description);
+		
 		$mail_body = 
 			"$sender_name е намерил грешка в книгата \"Въведение в програмирането с Java\".\n\n".
 			"Дата: $date\n".
@@ -62,7 +60,7 @@
 			"Изпратил (e-mail): $sender_email\n".
 			"Страница: $page_number\n".
 			"Ред (редове): $row_number\n".
-			"Изпращачът $agreed съгласен да му се публикува името.\n".
+			"Изпращачът $agree съгласен да му се публикува името.\n".
 			"Описание на грешката:\n\n$bug_description\n\n\n".
 			"See the bug here: http://www.nakov.com/introjavabook-bugs/show-bug.php?id=$bug_id\n";
 		$mail_recipients = MAIL_RECIPIENTS;
@@ -85,6 +83,8 @@
 		if ($bug == false) {
 			return false;
 		}
+
+		$comment = RemoveUnwantedSlashes($comment);
 		
 		$bug_sender_email = $bug['sender_email'];
 		$bug_date = $bug['date'];
@@ -131,7 +131,7 @@
 	}
 	
 	function GetBugById($bug_id) {
-		$bug_id = mysql_real_escape_string($bug_id);
+		$bug_id = MySQLEscapeString($bug_id);
 		$select_query = 
 			"SELECT bug_id, sent_by, sender_email, ".
 			"DATE_FORMAT(sent_on,'%e-%m-%Y %k:%i') as date, ".
@@ -149,7 +149,7 @@
 	
 	function GetAllCommentsForBug($bug_id)
 	{
-		$bug_id = mysql_real_escape_string($bug_id);
+		$bug_id = MySQLEscapeString($bug_id);
 		$select_query = 
 			"SELECT sent_by, DATE_FORMAT(sent_on,'%e-%m-%Y %k:%i') as date, comment ".
 			"FROM introjavabook_bug_comments ".
@@ -159,8 +159,21 @@
 		return $sql_result;	
 	}
 	
+	function RemoveUnwantedSlashes($text) {
+		if (get_magic_quotes_gpc()) {
+			$text = stripslashes($text);
+		}
+		return $text;
+	}
+
+	function MySQLEscapeString($text) {
+		$text_without_slashes = RemoveUnwantedSlashes($text);
+		$escaped_text = mysql_real_escape_string($text_without_slashes);
+		return $escaped_text;
+	}
+
 	function HtmlEscapeFormattedText($html_text) {
-		$result_html_text = new_textwrap($html_text, 100);
+		$result_html_text = TextWrap($html_text, 100);
 		$result_html_text = htmlspecialchars($result_html_text);
 		$result_html_text = str_replace(' ', '&nbsp;', $result_html_text);
 		$result_html_text = str_replace("\t", '&nbsp;&nbsp;&nbsp;&nbsp;', $result_html_text);
@@ -168,56 +181,73 @@
 		return $result_html_text;
 	}
 
-	function new_textwrap($String, $breaksAt = 78, $breakStr = "\n", $cut = 1 , $padStr="") {
-	/*
-		new_textwrap 1.0 <linker@toter-link.de>
-		
-		Based on textwrap 1.0 by Brian Moon <brian@phorum.org>
-		http://px.sklar.com/code-pretty.html?code_id=370
-		
-		tested with PHP 3.0.16 WIN
-		You can use it as ' wordwrap ' from PHP4
-		
-		$String    The string to be wrapped.
-		$breaksAt  How many characters each line should be.
-		$breakStr  What character should be used to cause a break.
-		$cut       If the cut is set to 1, the string is always wrapped at the specified width.
-		$padStr    Allows for the wrapped lines to be padded at the begining.
-	*/
-	
-		$newString="";
-		$lines=explode($breakStr, $String);
-		$cnt=count($lines);
-		for($x=0;$x<$cnt;$x++){
-		  if(strlen($lines[$x])>$breaksAt){
-		    $str=$lines[$x];
-		    while(strlen($str)>$breaksAt){
+	function TextWrap($string, $breaksAt = 78, $breakStr = "\n", $cut = 1 , $padStr="") {
+		/*
+			$string    The string to be wrapped.
+			$breaksAt  How many characters each line should be.
+			$breakStr  What character should be used to cause a break.
+			$cut       If the cut is set to 1, the string is always wrapped at the specified width.
+			$padStr    Allows for the wrapped lines to be padded at the begining.
+		*/
+			
+		$newString = "";
+		$lines = explode($breakStr, $string);
+		$cnt = count($lines);
+		for ($x=0; $x<$cnt; $x++) {
+		  if (strlen($lines[$x]) > $breaksAt) {
+		    $str = $lines[$x];
+		    while (strlen($str) > $breaksAt) {
 		      $find = 1 ;
-		      // echo $str."\n" ;
-		      $pos=strrpos(substr($str, 0, $breaksAt+1), " ");
+		      $pos = strrpos(substr($str, 0, $breaksAt+1), " ");
 		      if ($pos == false) {
-		          If($cut) {
-		              $pos = $breaksAt ;
-		              $find = 0 ;
-		          } else {
-		              $pos= strpos($str, " ");
-		              if ($pos == false)
-		                  break;
-		          }
+						if ($cut) {
+							$pos = $breaksAt ;
+							$find = 0 ;
+						} else {
+							$pos= strpos($str, " ");
+							if ($pos == false) {
+								break;
+							}
+						}
 		      }
-		      $newString.=$padStr.substr($str, 0, $pos).$breakStr;
-		      $str=(substr($str, $pos + $find)); // or $str=trim(substr($str, $pos));
-		
+		      $newString .= $padStr.substr($str, 0, $pos).$breakStr;
+		      $str = substr($str, $pos + $find);
 		    }
-		    $newString.=$padStr.$str.$breakStr;
+		    $newString .= $padStr . $str . $breakStr;
 		  }
 		  else{
-		    $newString.=$padStr.$lines[$x].$breakStr;
+		    $newString .= $padStr . $lines[$x] . $breakStr;
 		  }
 		}
-		
-		return substr ($newString,0, -(strlen($breakStr)));
+
+		$result = substr($newString, 0, -(strlen($breakStr)));
+		return $result;
+	}
 	
-	} // end new_textwrap()
+	function GetNumberAsWord($num) {
+		switch($num) {
+			case 1: return "едно";
+			case 2: return "две";
+			case 3: return "три";
+			case 4: return "четири";
+			case 5: return "пет";
+			case 6: return "шест";
+			case 7: return "седем";
+			case 8: return "осем";
+			case 9: return "девет";
+			case 10: return "десет";
+			case 11: return "единадесет";
+			case 12: return "дванадесет";
+			case 13: return "тринадесет";
+			case 14: return "четиринадесет";
+			case 15: return "петнадесет";
+			case 16: return "шестнадесет";
+			case 17: return "седемнадесет";
+			case 18: return "осемнадесет";
+			case 19: return "деветнадесет";
+			case 20: return "двадесет";
+			default: throw new Exception("GetNumberAsWord: invalid number"); 
+		}
+	}
 
 ?>
